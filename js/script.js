d2f8938f1d9cc2a0bf3809640836a7939b6c0488
malerba118 <malerba118@users.noreply.github.com>
2018-06-16 00:49:24 -0500

    Randomize more again

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function code(FileService, $q, $timeout) {
         return function() {
           return $timeout(function() {
             editor.insert(letter);
-          }, (~~(Math.random()*20))+60);
+          }, (~~(Math.random()*40))+45);
         }
       };
       var DEFAULT_HEIGHT = '200px';
